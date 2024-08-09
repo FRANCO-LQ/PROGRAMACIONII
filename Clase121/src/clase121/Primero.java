2f8ed8fcbb5079c9a12de1d4c9ad5a31fbcd574e
Franco858 <160808364+Franco858@users.noreply.github.com>
2024-08-09 17:37:11 -0400

    Add files via upload

---
+++ Clase121/src/clase121/Primero.java
@@ -0,0 +1,13 @@
+package clase121;
+
+public class Primero {
+    public static void main(String[] args) {
+        int A=10;
+         System.out.println("Bienvenido a INF-121 " +A); 
+         System.out.println("Franco");
+         System.out.println("Loza");
+         System.out.println("Quisbert");
+         System.out.println("Bienvenido Franco Loza Quisbert " +A);
+    }
+    
+}
